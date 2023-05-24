7cbef78e81210d12b3aaaa3d437d4b16f6e8b1cd
AdDraw <drawc.adam.priv@gmail.com>
2023-05-24 23:35:35 +0200

    flesh out first tb attempt

--- buffer_throghput_tb/tb_top.v
+++ buffer_throghput_tb/tb_top.v
@@ -1,28 +1,66 @@
 `timescale 1ns/1ps
 `default_nettype none
 module tb_top #(
-  parameter integer FIFO_DEPTH = 10,
-  parameter integer WRITE_FREQ = 1,
-  parameter integer READ_FREQ = 1
+  parameter integer FIFO_DEPTH_W = 2,
+  parameter integer WRITE_FREQ   = 1,
+  parameter integer READ_FREQ    = 1
 )(
+  input  wire rst_ni,
   input  wire clk_w_i,
   input  wire clk_r_i,
   input  wire we_i,
   input  wire re_i,
-  output wire fifo_empty_o,
-  output wire fifo_full_o
+  output wire rd_rdy_o,
+  output wire wr_rdy_o
 );
-
   generate
     if (WRITE_FREQ == READ_FREQ ) begin
       // Sync FIFO since read and write frequencies are identical
-      circ_fifo #() fifo_inst ( );
+      wire full;
+      wire empty;
+      circ_fifo #(
+        .DATA_W       (1),
+        .FIFO_DEPTH_W (FIFO_DEPTH_W),
+        .ID           (0)
+      ) fifo_inst (
+        .clk_i       (clk_w_i),
+        .rst_ni      (rst_ni),
+        .wr_en_i     (we_i),
+        .rd_en_i     (re_i),
+        .data_i      (1'b1),
+        .data_o      ( ),
+        .full_o      (full),
+        .empty_o     (empty),
+        .overflow_o  (),
+        .underflow_o ()
+      );
+      assign wr_rdy_o = ~full;
+      assign rd_rdy_o = ~empty;
     end
     else begin
+      wire rdata;
       // Async FIFO since read and write frequencies differ
-      cdc_fifo #() fifo_inst ();
+      async_fifo_Ndeep #(
+        .DATA_WIDTH         (1),
+        .BUFFER_DEPTH_POWER (FIFO_DEPTH_W )
+      ) fifo_inst (
+        .clk_a_i  (clk_w_i),
+        .a_rst_ni (rst_ni),
+        .a_we_i   (we_i),
+        .a_din_i  (1'b1),
+        .a_wrdy_o (wr_rdy_o),
+        .clk_b_i  (clk_r_i),
+        .b_rst_ni (rst_ni),
+        .b_re_i   (re_i),
+        .b_dout_o (rdata),
+        .b_rrdy_o (rd_rdy_o)
+      );
     end
   endgenerate
+
+  initial begin
+    $dumpvars(0);
+  end
 
 endmodule
 `default_nettype wire
--- buffer_throghput_tb/test.py
+++ buffer_throghput_tb/test.py
@@ -1,11 +1,19 @@
 import cocotb
 from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, ClockCycles
 
 W_CLOCK_PERIOD = 1
 R_CLOCK_PERIOD = 1
+
+@cocotb.test()
 async def test(dut):
   d = dut
-  cocotb.start_soon(Clock(d.clk_r_i, R_CLOCK_PERIOD).start(start_high=False))
-  cocotb.start_soon(Clock(d.clk_w_i, W_CLOCK_PERIOD).start(start_high=False))
+  cocotb.start_soon(Clock(d.clk_r_i, R_CLOCK_PERIOD, units="ns").start(start_high=False))
+  cocotb.start_soon(Clock(d.clk_w_i, W_CLOCK_PERIOD, units="ns").start(start_high=False))
 
-
+  d.re_i.value = 0
+  d.we_i.value = 0
+  d.rst_ni.value = 0
+  await ClockCycles(d.clk_w_i, 10)
+  d.rst_ni.value = 1
+  await ClockCycles(d.clk_w_i, 10)
